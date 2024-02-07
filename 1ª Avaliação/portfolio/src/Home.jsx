cfb7e5b4b10b5b5e915508d6d5da2812aa005373
João Pedro ⚡ <joaopedrobraga.07@gmail.com>
2024-02-07 00:23:26 -0300

    Includes Aula 2

--- 1ª Avaliação/portfolio/src/Home.jsx
+++ 1ª Avaliação/portfolio/src/Home.jsx
@@ -12,6 +12,32 @@ const Home = () =>{
             setExpandedItems([...expandedItems, itemId]);
         }
     };
+
+    function contarVogais() {
+        // Obtém o valor inserido no elemento de entrada HTML com o id 'inputImperativo'
+        let inputImperativo = document.getElementById('inputImperativo').value;
+    
+        // Inicializa a variável de contagem de vogais
+        let count = 0;
+    
+        // Define um array contendo as vogais
+        const vogais = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+    
+        // Itera sobre cada caractere da string inserida
+        for (let i = 0; i < inputImperativo.length; i++) {
+            let char = inputImperativo[i];
+            
+            // Verifica se o caractere atual é uma vogal
+            if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u' ||
+                char === 'A' || char === 'E' || char === 'I' || char === 'O' || char === 'U') {
+                // Se for uma vogal, incrementa o contador de vogais
+                count++;
+            }
+        }
+    
+        // Atualiza o conteúdo do elemento HTML com o id 'vogaisCount' para exibir o número de vogais encontrado
+        document.getElementById('vogaisCount').textContent = 'Número de vogais: ' + count;
+    }    
     
     return(
         <div>
@@ -49,41 +75,59 @@ const Home = () =>{
                         <span className="timeline__dot"></span>
                         <span id="item1-name" className="timeline__meta">
                             <time className="timeline__date" dateTime="1970-01-01">January 17, 2024</time><br/>
-                            <strong className="timeline__title">Aula 1</strong>
+                            <strong className="timeline__title">Paradigmas de Programação - Aula 1</strong>
                         </span>
                     </div>
                     <div className={`timeline__item-body ${expandedItems.includes('item1') ? 'timeline__item-body--expanded' : ''}`} id="item1-ctrld" role="region" aria-labelledby="item1" aria-hidden={!expandedItems.includes('item1')}>
                         <div className="timeline__item-body-content">
-                            <h3>Paradigmas de programação</h3>
-                            <h4>JavaScript</h4>
+                            
+                            <h3>JavaScript</h3>
                             <p className="timeline__item-p">
                             <strong>Imperativo:</strong> Descreve passo a passo como o programa deve executar as tarefas, usando instruções sequenciais e estruturas de controle.<br/>
+                                <strong>Ex:</strong> Desenvolvimento de um algoritmo de ordenação como o Bubble Sort. <br />
                             <strong>Declarativo:</strong> Define o que o programa deve realizar, focando nos resultados desejados em vez dos detalhes de implementação.<br/>
+                                <strong>Ex:</strong> Construção de uma interface de usuário (UI) utilizando bibliotecas como React.js, onde a estrutura e o comportamento são declarados por meio de componentes. <br />
                             <strong>Orientado a Objetos:</strong> Organiza o código em torno de objetos que contêm propriedades e métodos, facilitando a reutilização e a encapsulação de dados.<br/>
-                            <strong>Funcional:</strong> Trata as funções como cidadãs de primeira classe, permitindo que sejam passadas como argumentos e retornadas por outras funções, e enfatiza a imutabilidade e a composição de funções.<br/>
+                                <strong>Ex:</strong> Desenvolvimento de um sistema de gerenciamento de tarefas, onde cada tarefa é representada por um objeto com propriedades e métodos específicos. <br />
                             <strong>Event-Driven:</strong> Baseia-se na manipulação de eventos do usuário e do sistema, respondendo a esses eventos de forma assíncrona.<br/>
+                                <strong>Ex:</strong> Criação de um sistema de notificações em tempo real em uma aplicação web, onde eventos como novas mensagens ou atualizações são transmitidos e processados pelos clientes. <br />
                             <strong>Assíncrono:</strong> Lida com operações que não bloqueiam a execução do programa, utilizando callbacks, Promises e async/await para lidar com tarefas assíncronas de forma eficiente.<br/>
+                            <strong>Ex:</strong> Realização de chamadas de API assíncronas em uma aplicação web utilizando Promises ou async/await, permitindo que o programa continue executando outras tarefas enquanto aguarda a resposta da API. <br />
                             </p>
                         </div>
                     </div>
                 </div>
                 <div className="timeline__item">
                     <div className="timeline__item-header">
-                        <button className="timeline__arrow" type="button" id="item2" aria-labelledby="item2-name" aria-expanded="false" aria-controls="item2-ctrld" aria-haspopup="true" data-item="2">
+                        <button className="timeline__arrow"
+                        type="button"
+                        id="item2"
+                        aria-labelledby="item2-name"
+                        aria-expanded={expandedItems.includes('item2')}
+                        aria-controls="item2-ctrld"
+                        aria-haspopup="true"
+                        data-item="2"
+                        onClick={() => toggleItem('item2')}
+                        >
                             <svg className="timeline__arrow-icon" viewBox="0 0 24 24" width="24px" height="24px">
                                 <use href="#arrow" />
                             </svg>
                         </button>
                         <span className="timeline__dot"></span>
                         <span id="item2-name" className="timeline__meta">
-                            <time className="timeline__date" dateTime="1973-10-17">October 17, 1973</time><br/>
-                            <strong className="timeline__title">Digits Within ISO 8601 Format</strong>
+                            <time className="timeline__date" dateTime="1973-10-17">Janeiro 24, 2024</time><br/>
+                            <strong className="timeline__title"> Paradigmas Imperativos & Declarativos: Processamento de Strings - Aula 2</strong>
                         </span>
                     </div>
-                    <div className="timeline__item-body" id="item2-ctrld" role="region" aria-labelledby="item2" aria-hidden="true">
-                        <div className="timeline__item-body-content">
-                            <p className="timeline__item-p">At 6:36:57 PM UTC, the date in ISO 8601 format (1973-10-17) within the time digits (119731017) appeared for the first time.</p>
+                    <div className={`timeline__item-body ${expandedItems.includes('item2') ? 'timeline__item-body--expanded' : ''}`} id="item2-ctrld" role="region" aria-labelledby="item2" aria-hidden={!expandedItems.includes('item2')}>
+                        <div className="timeline__item-body-content inputBox">
+                            <span>Contagem de Vogais - Paradigma Imperativo</span>
+                            <input type="text" id="inputImperativo" placeholder="Digite uma string"/><br />
+                            <p className="timeline__item-p" id="vogaisCount">Número de vogais:</p>
+                            <button onClick={contarVogais}>Processar String</button>
+                            
                         </div>
+                        
                     </div>
                 </div>
                 <div className="timeline__item">
