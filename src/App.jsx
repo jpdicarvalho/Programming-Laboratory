ffecb4706ce18cb3cecdb31044bf359b8d1c147f
João Pedro ⚡ <joaopedrobraga.07@gmail.com>
2024-02-15 20:39:51 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
       <Router>
         <Routes>
           
-          <Route path='/' element={<Home/>}></Route>
+          <Route path='/Home' element={<Home/>}></Route>
         </Routes>
       </Router>
     </>
